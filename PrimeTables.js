2ffef568c7d038f088fd6856a2b52bc6e6a2c085
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-09-20 00:33:25 +0100

    Add table creation and filling functions
    The table creation function creates a 2D array based on the size of the prime number list. The filling function multiplies the prime numbers and stores the results in the 2D array.
    
    Also added module exports for IsPrime and iteration_loop so they can be accessed by the test file.

--- PrimeTables.js
+++ PrimeTables.js
@@ -1,11 +1,30 @@
+const { table } = require("node:console");
+
 let number_of_prime =0;
 let count =0;
+let N_prime_numbers = [];
+
+function creating_table(list_prime){
+  let Table = [];
+  for(let i =0; i < list_prime.length; i++){
+    Table[i] = [];
+  }
+  filling_table(Table,list_prime)
+}
+function filling_table(Table,list_prime){
+  for(let i = 0; i < list_prime.length; i++){
+    for(let j =0; j< list_prime.length; j++){
+        Table[i][j] = list_prime[i]* list_prime[j];
+    }
+  }
+  console.log(Table);
+}
 function iteration_loop(N){
   while(number_of_prime < N){
-      
+
       if(IsPrime(count) == true){
         number_of_prime+=1;
-        console.log(count);
+        N_prime_numbers.push(count);
         count+=1;
         IsPrime(count);
       }else{
@@ -13,6 +32,10 @@ function iteration_loop(N){
          IsPrime(count);
       }
   }
+
+  creating_table(N_prime_numbers);
+  return N_prime_numbers;
+  
 }
 
 function IsPrime(number) {
@@ -29,5 +52,5 @@ function IsPrime(number) {
 
 }
 
-// console.log(IsPrime(3));
-iteration_loop(10);
+iteration_loop(3);
+module.exports = {IsPrime,iteration_loop};
